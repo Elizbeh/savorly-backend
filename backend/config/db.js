7949f10cdd1513740d822c1c682228552aeeea07
Elizbeh <eliz01_01@yahoo.com>
2025-05-13 15:05:12 +0200

    Fix SSL issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+   ssl: {
+    rejectUnauthorized: true
+  },
   charset: 'utf8mb4',
   waitForConnections: true,
   connectTimeout: 30000,
