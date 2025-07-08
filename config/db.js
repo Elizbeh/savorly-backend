4504187fe5e1cf9fe2d468361d19341ca188469a
Elizbeh <eliz01_01@yahoo.com>
2025-07-08 22:29:29 +0200

    Test

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import mysql from 'mysql2';
 import logger from '../config/logger.js';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-
 const pool = mysql
   .createPool({
-    host: isProd ? process.env.DB_HOST : process.env.DB_HOST,
-  user: isProd ? process.env.DB_USER : process.env.DB_USER,
-  password: isProd ? process.env.DB_PASSWORD : process.env.DB_PASSWORD,
-  database: isProd ? process.env.DB_NAME : process.env.DB_NAME,
-    // SSL only for prod
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ...(isProd && {
       ssl: {
         rejectUnauthorized: true,
@@ -23,15 +21,18 @@ const pool = mysql
   })
   .promise();
 
-// quick connection check in dev
+// Quick connection check in dev
 if (!isProd) {
   (async () => {
     try {
       const conn = await pool.getConnection();
-      logger.info('Database connected successfully');
+      logger.info('✅ Database connected successfully');
       conn.release();
     } catch (err) {
-      logger.error('Database connection error', { message: err.message, stack: err.stack });
+      logger.error('❌ Database connection error', {
+        message: err.message,
+        stack: err.stack,
+      });
     }
   })();
 }
